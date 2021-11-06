1ec2f9de79a0fe6de7198e3862d57fbace7c23a3
Alkhimenok Kirill <alkhimenok.kirill.leonidovich@gmail.com>
2021-11-06 17:54:59 +0300

    add: npm init

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,5 @@
 var str = 'Hello world!';
+var num = 42;
 console.log(str);
+console.log(num);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 const str: string = 'Hello world!'
+const num: number = 42
 
 console.log(str)
+console.log(num)
