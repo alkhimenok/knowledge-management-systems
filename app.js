5b731c6bc85c080a3ea5d9849c37cdeeb5cc1e45
kirill-leonidovich <alkhimenok.kirill.leonidovich@gmail.com>
2021-11-22 20:28:52 +0300

    add: database conect

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const express = require('express')
 const path = require('path')
 const config = require('config')
+const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const PORT = config.get('port')
+const URL = config.get('databaseURL')
+
+const start = async () => {
+	try {
+		await mongoose.connect(URL)
+	} catch (e) {
+		console.log(e.message)
+		process.exit()
+	}
+}
+
+start().then(console.log('database conecked!'))
 
 app.use(express.static(path.resolve(__dirname, 'client', 'dist')))
 
